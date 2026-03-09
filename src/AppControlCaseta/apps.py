b620b700a451e9856c41f3ab9243299602401523
Francisco_HM <josefrancisco45rh@outlook.com>
2026-03-08 19:57:50 -0600

    se inicio la app de control caseta y se ajusto y  se manejo las urls de la app por separado

---
+++ src/AppControlCaseta/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppcontrolcasetaConfig(AppConfig):
+    name = 'AppControlCaseta'
---
+++ src/AppControlCaseta/urls.py
@@ -0,0 +1,6 @@
+from AppControlCaseta import views
+
+# aqui es donde unicamente se manejaran las urls de la AppControlCaseta
+urlpatterns = [
+
+]
